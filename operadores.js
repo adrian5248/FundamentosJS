f076a6f81f011b51ab42d60cf06b22fdbffec43f
Adrian <adrian.duran.matias@gmail.com>
2023-09-08 15:05:48 -0600

    03 operadores JS

---
+++ operadores.js
@@ -0,0 +1,38 @@
+// Operadores Matematicos
+
+console.log(1 + 1)
+console.log(1 - 1)
+console.log(1 * 1)
+console.log(1 / 1)
+console.log(1 % 1)
+console.log(3 ** 3)
+
+// Mas operadores
+let a = 10;
+a += 10;
+console.log(a);
+a += 10; // a = a + 10
+console.log(a);
+a += 10; // a = a - 10
+console.log(a);
+a += 10; // a = a * 10
+console.log(a);
+a += 10; // a = a / 10
+console.log(a);
+a += 10; // a = a % 10
+
+
+// Operadores relacionales 
+console.log (10 < 10);
+console.log (10 <= 10);
+console.log (10 > 10);
+console.log (10 >= 10);
+console.log (10 !== 10);
+console.log (10 === 10);
+
+// operadores logicos
+console.log(true && true);
+console.log(true || true);
+console.log(!true);
+
+
---
+++ strings.js
@@ -0,0 +1,7 @@
+let name = "Adrian";
+let lastname = "Duran";
+
+console.log("soy" + name + 'y mi apellido es ' + lastname);
+
+console.log("I'm" + name + " " + lastname);
+console.log("I\'m" + name + " " + lastname);
