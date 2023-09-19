85405cec095974aca43106a4f9301d4d7c4febf4
Adrian <adrian.duran.matias@gmail.com>
2023-09-19 11:38:14 -0600

    08 Arrays

---
+++ arrays.js
@@ -0,0 +1,38 @@
+// Arrays en Javascript
+const miArray = [1, 2, 4, true, 'Hello', 'A', 2.1, [1, 1]]
+console.log(miArray);
+console.log(miArray[0]);
+console.log(miArray[1]);
+miArray[0] = "One";
+miArray[3] = false;
+console.log(miArray);
+
+
+// Pila en JS
+// Lat in first out 
+// Ingresar valores al final 
+// Extraer valores del final 
+// pop, shift, unshift, push
+const pila = [];
+// Insertar con push
+pila.push(1);
+pila.push(2);
+pila.push(3);
+console.log(pila);
+console.log(pila.pop());
+console.log(pila);
+console.log(pila.pop());
+console.log(pila);
+
+// cola en JS
+// first in first out
+const cola = []
+cola.unshift(1);
+cola.unshift(2);
+cola.unshift(3);
+console.log(cola);
+console.log(cola.shift());
+console.log(cola);
+console.log(cola.shift());
+console.log(cola);
+
