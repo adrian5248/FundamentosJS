b392874aba7bcc0106bd62a4ce9af45630e1c160
Adrian <adrian.duran.matias@gmail.com>
2023-09-27 10:49:44 -0600

    16 DOM Galeria Booststrap

---
+++ dom/galeria.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+const image = document.querySelector('#image');
+
+function setImage(e){
+    const imageSrc = e.target.src;
+    image.src = imageSrc;
+    
+}
+
+container.addEventListener('click', setImage);
